efd16a95c31f046527888257f9f4655b47c5e7f8
rogelioalejandro <rogelio.fonteboa@ntsprint.com>
2022-08-21 13:38:16 +0200

    Initial Commit

---
+++ src/utils/constants.js
@@ -0,0 +1,12 @@
+// API
+export const API = "https://pokeapi.co/api/v2/";
+export const ROUTES = {
+  getAllPokemons: "pokemon",
+};
+
+// Pokemons Filter Component
+export const FILTER_PLACEHOLDER = "Find some Pokemons...";
+
+// Pokemons Table Component
+export const NO_RESULTS =
+  "There is no information related to the Pokemon you are looking for 🤓";
